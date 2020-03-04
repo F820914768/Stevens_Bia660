ce24bec502670dc29716f045ba7a508362afe975
F820914768 <820914768@qq.com>
2020-03-04 17:14:06 -0500

    define pipeline to download files

---
+++ HW5/pipeline.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+
+            
+
+class Pipeline:
+    def __init__(self, init_url, num_books, queue, 
+                 crawler, max_depth):
+        self.num_books = num_books
+        self.queue = queue
+        self.crawler = crawler
+        self.queue.put(init_url)
+        self.max_depth = max_depth
+        
+    def run(self):
+        url, depth = self.queue.get()
+        depth += 1
+        page = self.crawler.get(url)
+        hrefs = self.crawler.parse_url(page, depth,
+                                       self.max_depth)
+        if hrefs == None:
+            return None
+        for href in hrefs:
+            self.queue.put_after_filter(href, depth)
+        
+
+        
+
+
+
+
